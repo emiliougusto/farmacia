55b600b391476d2eaf8de7ae858138cc6abed8e7
Emilio <emiliogremiocolorado@gmail.com>
2025-08-28 15:33:56 -0300

    Criação do Hello World e inicio da API Farmacia

---
+++ Emilio/Emilio/src/main/java/com/farmacia/Emilio/model/Produtos.java
@@ -0,0 +1,25 @@
+package com.farmacia.Emilio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity(name = "TBPRODUTOS")
+
+
+public class Produtos {
+  @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CDPRODUTO")
+    private Integer cdProduto;
+  @Column(name = "NMPRODUTO")
+    private String nmProduto;
+  @Column(name="DSBULA")
+    private String dsBula;
+    @Column(name ="VLPRODUTO")
+    protected Integer vlProduto;
+}
